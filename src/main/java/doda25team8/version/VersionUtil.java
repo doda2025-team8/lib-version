0f2af57ca14ab719ccc4be465a7f45ac9cc46e26
[Yuchen Sun] <[syc617185828@gmail.com]>
2025-12-18 13:13:27 +0100

    updated the auto-release, and added the branch pre-release

--- src/main/java/doda25team8/version/VersionUtil.java
+++ src/main/java/doda25team8/version/VersionUtil.java
@@ -1,11 +1,15 @@
 package doda25team8.version;
+
+import java.util.Properties;
+
 public final class VersionUtil {
-    public static String getVersion(){
-        String version = VersionUtil.class.getPackage().getImplementationVersion();
-        if(version != null){
-            return version;
-        } else {
-            return "DEV";
+    public static String getVersion() {
+        try {
+            Properties p = new Properties();
+            p.load(VersionUtil.class.getResourceAsStream("/META-INF/maven/doda25team8/lib-version/pom.properties"));    // change to read the SSOT version from the pom.properties
+            return p.getProperty("version");
+        } catch (Exception e) {
+            return "unknown-dev"; 
         }
     }
 }
